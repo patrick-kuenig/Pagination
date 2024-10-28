3e25aed449a30757ffd2b7de051f96d11024511a
patrick-kuenig <kuenig.patrick@gmail.com>
2024-10-28 23:55:55 +0300

    27_10_2024

--- Module_19_Pagination/Pagination/views.py
+++ Module_19_Pagination/Pagination/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
 from django.http import request
 
 
 # Create your views here.
 class IndexView(ListView):
-    try:
-        paginate_by = request.GET['dropdown']
-    except:
-        paginate_by = 1
     model = Post
     template_name = 'index.html'
+
+    def get_paginate_by(self, queryset):
+        if self.request.GET.get('paginate_by', self.paginate_by) is None:
+            return 2
+        else:
+            return self.request.GET.get('paginate_by', self.paginate_by)
+
+
+
